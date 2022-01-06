e058cf272c25b638edf4ea64c5f5bd949a9d254d
Anton Iudin <yudin431@gmail.com>
2022-01-06 15:36:17 -0500

    Create angular project

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from './task.service';
+
+@Component({
+  selector: 'task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  title = "This part works";
+  parts;
+  
+  constructor(service: TaskService) {
+    this.parts = service.getTasks();
+    
+  }
+}
---
+++ src/app/task/task.service.ts
@@ -0,0 +1,5 @@
+export class TaskService {
+    getTasks() {
+        return ["part1", "part2", "part3"];
+    }
+}
